aee84e4e504d94e3f2023166416ed28d38ba0971
IvanDobrovolsky <dobrovolsky94@gmail.com>
2017-04-09 10:52:14 +0300

    Migration/Dev: migrated HeaderComponent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
 import headerTemplate from './header.component.html'
 
-class HeaderComponent {
-    public onAdd: () => void;
-    public static $inject = [];
+@Component({
+    selector: 'ssw-header',
+    template: headerTemplate
+})
+export class HeaderComponent {
+    @Output() add = new EventEmitter<void>();
 
     public addNewTask(): void {
-        this.onAdd();
+        this.add.emit();
     }
 }
 
-export const headerComponent = {
-    bindings: {
-        onAdd: "&"
-    },
-    controller: HeaderComponent,
-    template: headerTemplate
-};
